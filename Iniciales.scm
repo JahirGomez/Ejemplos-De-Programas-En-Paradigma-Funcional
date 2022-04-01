94f28c19729d2244798c39ae03565ad911357da4
JahirGomez <86177245+JahirGomez@users.noreply.github.com>
2022-03-31 21:24:33 -0600

    Update Iniciales.scm

--- Iniciales.scm
+++ Iniciales.scm
@@ -1,27 +1,10 @@
 #lang racket
-(define (toString0 a)
-  (string-ref (~a a) 0)
-)
-
-(define (toString1 a)
-  (string-ref (~a a) 1)
-)
-
-(define (in lista1 lista2)
-  (if (null? lista1)
-    (print lista2)
-    (cond
-    ((equal? (string-ref (~a lista1) 0) "(") ((in (cdr lista1) (list lista2 (toString1 (car lista1))))))
-    (else  (in (cdr lista1) (list lista2 (toString0 (car lista1)))))
-    )
-  )
-)
-
 (define (iniciales lista)
-  (if (null? lista)
-    (print lista)
-    (in lista '())
+  (map (lambda (caracter)
+         (string->symbol
+          (string
+           (string-ref
+            (symbol->string caracter) 0))))
+       lista)
   )
-)
-
 (iniciales '(Lenguajes y Paradigmas de Programación))
